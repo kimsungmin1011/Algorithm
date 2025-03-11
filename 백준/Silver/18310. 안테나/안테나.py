d0cf2f2c0db801c931f3a788966bda77d596a255
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-11 16:10:52 +0900

    [Silver III] Title: 안테나, Time: 152 ms, Memory: 140532 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.py
+++ 백준/Silver/18310. 안테나/안테나.py
@@ -2,26 +2,11 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-house = list(map(int, input().split()))
-house.sort()
+n = int(input())  # 집의 개수 입력
+house = list(map(int, input().split()))  # 집들의 위치 입력
+house.sort()  # 집의 위치를 오름차순 정렬
 
-if n % 2 != 0:
-    print(house[n // 2])
-else:
-    first = house[n // 2 - 1]
-    fd = 0
-    for i in range(n):
-        if i != n // 2 - 1:
-            fd += abs(first - house[i])
-
-    second = house[n // 2]
-    sd = 0
-    for i in range(n):
-        if i != n // 2:
-            sd += abs(second - house[i])
-
-    if fd <= sd:
-        print(first)
-    else:
-        print(second)
+if n % 2 != 0:  # 집의 개수가 홀수일 경우
+    print(house[n // 2])  # 중앙값 출력
+else:  # 집의 개수가 짝수일 경우
+    print(house[n // 2 - 1])  # 중앙값 출력
