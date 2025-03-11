a788dace1dd24b75ae2693f8b2154a42b0b894c0
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-11 18:40:31 +0900

    [Gold V] Title: 회의실 배정, Time: 288 ms, Memory: 116392 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -7,15 +7,15 @@ N = int(input())
 timeline = []
 for i in range(N):
     start, end = map(int, input().split())
-    timeline.append((start, end))
+    timeline.append((end, start))
 
-timeline.sort(key=lambda x: (x[1], x[0]))
+timeline.sort()
 count = 0
 end = 0
 
 for i in range(N):
-    if timeline[i][0] >= end:
-        end = timeline[i][1]
+    if timeline[i][1] >= end:
+        end = timeline[i][0]
         count += 1
 
 print(count)
