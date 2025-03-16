1006813428a472dc0060127ec3f62debaa226995
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-16 20:38:37 +0900

    [Silver IV] Title: 보물, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,11 +1,11 @@
 n = int(input())
-
 a = list(map(int, input().split()))
-a.sort()
 b = list(map(int, input().split()))
+a.sort()
 b.sort(reverse=True)
 
 answer = 0
+
 for i in range(n):
     answer += a[i] * b[i]
 
