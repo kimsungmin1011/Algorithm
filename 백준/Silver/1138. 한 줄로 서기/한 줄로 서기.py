6b20289bf0f96a8de916e6dc26ef8b52ee5e678c
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-16 22:40:52 +0900

    [Silver II] Title: 한 줄로 서기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,16 +1,15 @@
 n = int(input())
 line = list(map(int, input().split()))
-rank = [0] * n
+rank_list = [0] * n
 
 for i in range(n):
     number = line[i]
     count = 0
     for x in range(n):
-        if count == number:
-            if rank[x] == 0:
-                rank[x] = i + 1
-                break
-        if rank[x] == 0 or rank[x] > i + 1:
+        if count == number and rank_list[x] == 0:
+            rank_list[x] = i + 1
+            break
+        elif rank_list[x] == 0 or rank_list[x] > i + 1:
             count += 1
 
-print(*rank)
+print(*rank_list)
