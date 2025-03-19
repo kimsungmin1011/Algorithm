6ca6c8359c0aa0e8874da65c4780e47e9e8682bd
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-19 10:47:51 +0900

    [Gold V] Title: 센서, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -1,17 +1,15 @@
 n = int(input())
 k = int(input())
-
 sensor = list(map(int, input().split()))
 sensor.sort()
 
 distance = []
-
 for i in range(n - 1):
-    distance.append(abs(sensor[i] - sensor[i + 1]))
+    distance.append(sensor[i + 1] - sensor[i])
 
 distance.sort()
 
-for _ in range(k - 1):
+for i in range(k - 1):
     if len(distance) >= 1:
         distance.pop()
 
