b6b016e781971fdd0ca79359e6cd6855469b987d
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-24 19:13:56 +0900

    [level 2] Title: 기능개발, Time: 0.46 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,9 +1,5 @@
-from collections import deque
-
-
 def solution(progresses, speeds):
     answer = []
-    # p_queue = deque(progresses)
 
     while len(progresses) != 0:
         count = 0
@@ -12,7 +8,6 @@ def solution(progresses, speeds):
 
         while True:
             if len(progresses) > 0 and progresses[0] >= 100:
-                # p_queue.popleft()
                 progresses.pop(0)
                 speeds.pop(0)
                 count += 1
