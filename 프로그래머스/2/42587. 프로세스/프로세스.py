468736f62e61ddc7b0b029973c6bef7c82166728
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-24 18:39:02 +0900

    [level 2] Title: 프로세스, Time: 2.42 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,18 +1,17 @@
+from collections import deque
+
+
 def solution(priorities, location):
-    answer = 1
-    visited = [False for _ in range(len(priorities))]
+    queue = deque([(i, p) for i, p in enumerate(priorities)])
+    answer = 0
 
-    while True:
-        for i in range(len(priorities)):
-            max_value = 0
-            for i2 in range(len(priorities)):
-                if visited[i2] == False:
-                    max_value = max(max_value, priorities[i2])
+    while queue:
+        current = queue.popleft()
 
-            if visited[i] == False and priorities[i] == max_value:
-                if i == location:
-                    visited[i] = True
-                    return answer
-
-                visited[i] = True
-                answer += 1
+        # 중요도가 더 높은 프로세스가 뒤에 있다면, 현재 프로세스를 뒤로 보냄
+        if queue and max(q[1] for q in queue) > current[1]:
+            queue.append(current)
+        else:
+            answer += 1
+            if current[0] == location:
+                return answer
