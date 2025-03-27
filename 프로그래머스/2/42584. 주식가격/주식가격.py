59ca18fa683eef34e69cb14aea2aca091e1f0a77
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-03-27 11:07:56 +0900

    [level 2] Title: 주식가격, Time: 127.10 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,13 @@
 def solution(prices):
     answer = []
 
-    for i in range(len(prices) - 1):
+    for i in range(len(prices)):
         current = prices[i]
-        count = 1
-        for i2 in range(i + 1, len(prices) - 1):
-            if prices[i2] >= current:
-                count += 1
-            else:
+        count = 0
+        for i2 in range(i + 1, len(prices)):
+            count += 1
+            if prices[i2] < current:
                 break
         answer.append(count)
-    answer.append(0)
 
     return answer
