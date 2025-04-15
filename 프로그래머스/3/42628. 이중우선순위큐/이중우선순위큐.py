33b53416462e8b87e655302265c6672bbe571c40
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-15 13:48:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.40 ms, Memory: 22.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,23 +1,45 @@
 import heapq
 
-
 def solution(operations):
-    answer = []
+    min_heap = []
+    max_heap = []
+    visited = dict()  # key: 고유 ID, value: 유효 여부
+    idx = 0  # 고유 ID
 
-    for i in operations:
-        i = list(i.split(" "))
-        if i[0] == "I":
-            heapq.heappush(answer, int(i[1]))
-        else:
-            if answer:
-                if i[1] == "-1":
-                    heapq.heappop(answer)
-                elif i[1] == "1":
-                    answer.sort()
-                    answer.pop()
+    for op in operations:
+        cmd, val = op.split()
+        val = int(val)
 
-    if answer:
-        return [max(answer), min(answer)]
-    else:
+        if cmd == "I":
+            heapq.heappush(min_heap, (val, idx))
+            heapq.heappush(max_heap, (-val, idx))
+            visited[idx] = True
+            idx += 1
+
+        elif cmd == "D":
+            if val == 1:
+                # 최대값 제거
+                while max_heap and not visited.get(max_heap[0][1], False):
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+
+            elif val == -1:
+                # 최소값 제거
+                while min_heap and not visited.get(min_heap[0][1], False):
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+
+    # 동기화: 아직 유효한 원소만 찾기
+    while min_heap and not visited.get(min_heap[0][1], False):
+        heapq.heappop(min_heap)
+    while max_heap and not visited.get(max_heap[0][1], False):
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap:
         return [0, 0]
 
+    return [-max_heap[0][0], min_heap[0][0]]
