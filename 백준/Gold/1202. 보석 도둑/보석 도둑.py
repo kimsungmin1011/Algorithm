97ba9c8c3f0f48f6886e7d4c39804ca2eabb2fd8
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-18 11:34:38 +0900

    [Gold II] Title: 보석 도둑, Time: 1056 ms, Memory: 158956 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,24 +1,36 @@
 import heapq
+import sys
+
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
-jewelry = [tuple(map(int, input().split())) for _ in range(n)]
+jewels = []
+for _ in range(n):
+    m, v = map(int, input().split())
+    jewels.append((m, v))  # (무게, 가치)
+
 bags = [int(input()) for _ in range(k)]
 
-# 보석은 무게 기준 오름차순 정렬
-jewelry.sort()
-# 가방도 오름차순 정렬
+# 1) 보석을 무게 기준 오름차순 정렬
+jewels.sort()
+
+# 2) 가방을 용량 기준 오름차순 정렬
 bags.sort()
 
-result = 0
-heap = []
-j = 0  # 보석 인덱스
+max_heap = []
+idx = 0
+answer = 0
 
-for bag in bags:
-    # 현재 가방에 담을 수 있는 보석들을 최대 힙에 추가
-    while j < n and jewelry[j][0] <= bag:
-        heapq.heappush(heap, -jewelry[j][1])  # 가치 기준 최대 힙
-        j += 1
-    if heap:
-        result += -heapq.heappop(heap)
+# 3) 각 가방에 대해
+for capacity in bags:
+    #  - 해당 가방에 넣을 수 있는 모든 보석을 max-heap에 추가
+    while idx < n and jewels[idx][0] <= capacity:
+        # heapq는 최소 힙이므로, 가치에 -를 붙여 최대 힙처럼 사용
+        heapq.heappush(max_heap, -jewels[idx][1])
+        idx += 1
 
-print(result)
+    #  - 힙에서 가장 가치가 큰 보석을 꺼내어 더함
+    if max_heap:
+        answer += -heapq.heappop(max_heap)
+
+print(answer)
