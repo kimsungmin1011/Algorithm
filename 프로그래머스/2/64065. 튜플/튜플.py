c61a5a20675bd94eec9ece3c2fea1ca584aa160b
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-22 15:26:35 +0900

    [level 2] Title: 튜플, Time: 41.82 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,37 +1,15 @@
 def solution(s):
-    line = []
-    s = s.strip()
-    flag = False
+    s1 = s.lstrip("{").rstrip("}").split("},{")
+    sets = [list(map(int, x.split(","))) for x in s1]
+    sets.sort(key=len)
 
-    yoso = []
-    wonso = []
+    answer = []
+    seen = set()
 
-    # 문자열 파싱
-    for i in range(1, len(s) - 1):
-        if s[i] == ",":
-            if wonso:
-                yoso.append(int("".join(wonso[:])))
-                wonso = []
-            else:
-                continue
-        elif s[i] == "{":
-            flag = True
-        elif s[i] == "}":
-            yoso.append(int("".join(wonso[:])))
-            wonso = []
-            flag = False
-            line.append(yoso[:])
-            yoso = []
-        elif flag == True:
-            wonso.append(s[i])
-
-    # 길이를 기준으로 정렬
-    line = sorted(line, key=lambda x: len(x))
-    answer = [line[0][0]]  # 첫 번째 요소의 원소 넣기
-
-    for i in range(len(line)):
-        for j in line[i]:
-            if j not in answer:  # 현재 원소가 answer에 없다면
-                answer.append(j)
+    for group in sets:
+        for num in group:
+            if num not in seen:
+                seen.add(num)
+                answer.append(num)
 
     return answer
