2de48e988bb96383e0cd1f0cfde9ac33e25a4053
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-22 12:46:04 +0900

    [level 2] Title: 튜플, Time: 509.36 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,34 +1,37 @@
 def solution(s):
-    answer = []
+    line = []
     s = s.strip()
     flag = False
 
-    imsi = []
-    save = []
+    yoso = []
+    wonso = []
+
+    # 문자열 파싱
     for i in range(1, len(s) - 1):
         if s[i] == ",":
-            if save:
-                imsi.append(int("".join(save[:])))
-                save = []
+            if wonso:
+                yoso.append(int("".join(wonso[:])))
+                wonso = []
             else:
                 continue
         elif s[i] == "{":
             flag = True
         elif s[i] == "}":
-            imsi.append(int("".join(save[:])))
-            save = []
+            yoso.append(int("".join(wonso[:])))
+            wonso = []
             flag = False
-            answer.append(imsi[:])
-            imsi = []
+            line.append(yoso[:])
+            yoso = []
         elif flag == True:
-            save.append(s[i])
+            wonso.append(s[i])
 
-    answer = sorted(answer, key=lambda x: len(x))
-    last = [answer[0][0]]
+    # 길이를 기준으로 정렬
+    line = sorted(line, key=lambda x: len(x))
+    answer = [line[0][0]]  # 첫 번째 요소의 원소 넣기
 
-    for i in range(1, len(answer)):
-        for j in answer[i]:
-            if j not in last:
-                last.append(j)
+    for i in range(len(line)):
+        for j in line[i]:
+            if j not in answer:  # 현재 원소가 answer에 없다면
+                answer.append(j)
 
-    return last
+    return answer
