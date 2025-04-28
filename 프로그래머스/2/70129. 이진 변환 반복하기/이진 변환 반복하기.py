561ae0a224cd8c0f7a62da5e1563d29b23c5d428
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-28 12:58:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 29.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,11 +1,11 @@
 def solution(s):
     s = [int(i) for i in s]
-    big_s = []
+    new_s = []
     count = 0
     kill_zero = 0
 
     while True:
-        if len(s) == 1 and s[0] == 1:
+        if len(s) == 1:
             return [count, kill_zero]
 
         len_s = 0
@@ -15,13 +15,13 @@ def solution(s):
             else:
                 kill_zero += 1
 
-        big_s = []
+        new_s = []
         while len_s > 0:
             mok = len_s // 2
             namuji = len_s % 2
-            big_s.append(namuji)
+            new_s.append(namuji)
             len_s = mok
 
-        big_s.reverse()
+        new_s.reverse()
+        s = new_s
         count += 1
-        s = big_s
