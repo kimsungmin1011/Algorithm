d4b1498db93f5e452ae988533c6b1b680f323f1e
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-28 14:36:16 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,27 @@
-from collections import deque
-
-def bfs(graph,start,visited):
-    queue = deque([start])
-    visited[start] = True
-    infected = 0
-    while queue:
-        v = queue.popleft()
-        for i in sorted(graph[v]):
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-                infected += 1
-    return infected
-                
 n = int(input())
-graph = [[] for _ in range(n+1)]
 m = int(input())
 
+graph = [[] for _ in range(n + 1)]
+
 for _ in range(m):
-    a,b = map(int,input().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-visited = [False] * (n+1)
-print(bfs(graph,1,visited))
+visited = [False for _ in range(n + 1)]
+visited[1] = True
+count = 0
+
+
+def dfs(node):
+    global count
+    for next in graph[node]:
+        if visited[next] == False:
+            visited[next] = True
+            count += 1
+            dfs(next)
+
+
+dfs(1)
+
+print(count)
