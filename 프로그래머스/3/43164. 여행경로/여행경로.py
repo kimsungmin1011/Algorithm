c60b768ae30d5efb60347091bfffb868a45e754f
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-04-30 20:19:59 +0900

    [level 3] Title: 여행경로, Time: 219.69 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,6 +1,6 @@
 def solution(tickets):
-    course = {}
-    visited = {}
+    course = {}  # 출발지 → 도착지 리스트
+    visited = {}  # 출발지 → 도착지별 방문 여부 (항공권 기준!)
 
     for i, j in tickets:
         if i not in course:
@@ -18,21 +18,22 @@ def solution(tickets):
             for next in range(len(course[current])):
                 next_airport = course[current][next]
                 if visited[current][next] == False:
-                    visited[current][next] = True
-                    route.append(next_airport)
-                    dfs(next_airport)
-                    visited[current][next] = False
-                    route.pop()
+                    visited[current][next] = True        # 항공권 사용 체크
+                    route.append(next_airport)           # 경로에 추가
+                    dfs(next_airport)                    # 다음 공항 탐색
+                    visited[current][next] = False       # 항공권 사용 복구
+                    route.pop()                          # 경로 복구
 
+        # 모든 항공권을 사용했는지 확인
         flag = True
         for i in visited:
-            if False in visited[i]:
+            if False in visited[i]:                      # 하나라도 안 쓴 항공권이 있다면
                 flag = False
                 break
 
         if flag == True:
-            total_route.append(route[:])
+            total_route.append(route[:])                 # 완성된 경로 저장
 
     dfs("ICN")
 
-    return sorted(total_route)[0]
+    return sorted(total_route)[0]                        # 사전순 가장 앞선 경로 반환
