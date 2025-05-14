477e571d9c0b2b0eb42ddb933790330c4998f2ad
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-05-14 17:37:34 +0900

    [Gold IV] Title: 두 동전, Time: 112 ms, Memory: 113500 KB -BaekjoonHub

---
+++ 백준/Gold/16197. 두 동전/두 동전.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(input()))
+
+visited = set()
+
+first_location = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == "o":
+            first_location.append((i, j))
+
+visited.add(tuple(sorted(first_location)))
+queue = deque([(*sorted(first_location), 0)])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+flag = False
+while queue:
+    first_coin, second_coin, time = queue.popleft()
+
+    if flag == True or time >= 10:
+        break
+
+    fx, fy = first_coin[0], first_coin[1]
+    sx, sy = second_coin[0], second_coin[1]
+
+    for i in range(4):
+        nfx, nfy, nsx, nsy = fx + dx[i], fy + dy[i], sx + dx[i], sy + dy[i]
+        if nfx >= n or nfx < 0 or nfy >= m or nfy < 0:
+            if 0 <= nsx < n and 0 <= nsy < m:
+                flag = True
+                print(time + 1)
+                break
+
+        elif nsx >= n or nsx < 0 or nsy >= m or nsy < 0:
+            if 0 <= nfx < n and 0 <= nfy < m:
+                flag = True
+                print(time + 1)
+                break
+
+        elif 0 <= nfx < n and 0 <= nfy < m and 0 <= nsx < n and 0 <= nsy < m:
+            if graph[nfx][nfy] != "#" and graph[nsx][nsy] != "#":
+                if tuple(sorted(((nfx, nfy), (nsx, nsy)))) not in visited:
+                    visited.add(tuple(sorted(((nfx, nfy), (nsx, nsy)))))
+                    queue.append((*sorted(((nfx, nfy), (nsx, nsy))), time + 1))
+
+            elif graph[nfx][nfy] != "#":
+                if tuple(sorted(((nfx, nfy), (sx, sy)))) not in visited:
+                    visited.add(tuple(sorted(((nfx, nfy), (sx, sy)))))
+                    queue.append((*sorted(((nfx, nfy), (sx, sy))), time + 1))
+
+            elif graph[nsx][nsy] != "#":
+                if tuple(sorted(((fx, fy), (nsx, nsy)))) not in visited:
+                    visited.add(tuple(sorted(((fx, fy), (nsx, nsy)))))
+                    queue.append((*sorted(((fx, fy), (nsx, nsy))), time + 1))
+
+if flag == False:
+    print(-1)
