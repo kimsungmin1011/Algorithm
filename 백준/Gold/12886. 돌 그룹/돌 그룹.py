b5ba5ee25f38598ca578df062d034d4b8fc3e49d
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-05-20 10:43:30 +0900

    [Gold IV] Title: 돌 그룹, Time: 1924 ms, Memory: 340520 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.py
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -1,50 +1,44 @@
 from collections import deque
 
 a, b, c = map(int, input().split())
-
+visited = set()
+visited.add((a, b, c))
 queue = deque([(a, b, c)])
-visited = {}
-visited[str(a) + str(b) + str(c)] = 1
+flag = False
 
+while queue:
+    x, y, z = queue.popleft()
+    if x == y == z:
+        flag = True
+        print(1)
+        break
 
-def bfs():
-    while queue:
-        ca, cb, cc = queue.popleft()
+    if x > y:
+        if (x - y, y + y, z) not in visited:
+            visited.add((x - y, y + y, z))
+            queue.append(((x - y, y + y, z)))
+    elif x < y:
+        if (x + x, y - x, z) not in visited:
+            visited.add((x + x, y - x, z))
+            queue.append(((x + x, y - x, z)))
 
-        if ca == cb == cc:
-            return 1
+    if x > z:
+        if (x - z, y, z + z) not in visited:
+            visited.add((x - z, y, z + z))
+            queue.append(((x - z, y, z + z)))
+    elif x < z:
+        if (x + x, y, z - x) not in visited:
+            visited.add((x + x, y, z - x))
+            queue.append(((x + x, y, z - x)))
 
-        if ca != cb:
-            if ca > cb and str(ca - cb) + str(cb + cb) + str(cc) not in visited.keys():
-                visited[str(ca - cb) + str(cb + cb) + str(cc)] = 1
-                queue.append((ca - cb, cb + cb, cc))
-            elif (
-                cb > ca and str(ca + ca) + str(cb - ca) + str(cc) not in visited.keys()
-            ):
-                visited[str(ca + ca) + str(cb - ca) + str(cc)] = 1
-                queue.append((ca + ca, cb - ca, cc))
+    if y > z:
+        if (x, y - z, z + z) not in visited:
+            visited.add((x, y - z, z + z))
+            queue.append(((x, y - z, z + z)))
+    elif y < z:
+        if (x, y + y, z - y) not in visited:
+            visited.add((x, y + y, z - y))
+            queue.append(((x, y + y, z - y)))
 
-        if ca != cc:
-            if ca > cc and str(ca - cc) + str(cb) + str(cc + cc) not in visited.keys():
-                visited[str(ca - cc) + str(cb) + str(cc + cc)] = 1
-                queue.append((ca - cc, cb, cc + cc))
-            elif (
-                cc > ca and str(ca + ca) + str(cb) + str(cc - ca) not in visited.keys()
-            ):
-                visited[str(ca + ca) + str(cb) + str(cc - ca)] = 1
-                queue.append((ca + ca, cb, cc - ca))
-
-        if cb != cc:
-            if cb > cc and str(ca) + str(cb - cc) + str(cc + cc) not in visited.keys():
-                visited[str(ca) + str(cb - cc) + str(cc + cc)] = 1
-                queue.append((ca, cb - cc, cc + cc))
-            elif (
-                cc > cb and str(ca) + str(cb + cb) + str(cc - cb) not in visited.keys()
-            ):
-                visited[str(ca) + str(cb + cb) + str(cc - cb)] = 1
-                queue.append((ca, cb + cb, cc - cb))
-
-    return 0
-
-
-print(bfs())
+if flag == False:
+    print(0)
