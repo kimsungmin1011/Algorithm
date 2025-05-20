44926ddf58e2d939581c991e151b801f816e36f1
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-05-20 10:53:57 +0900

    [Gold IV] Title: 돌 그룹, Time: 708 ms, Memory: 170848 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.py
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -2,7 +2,7 @@ from collections import deque
 
 a, b, c = map(int, input().split())
 visited = set()
-visited.add((a, b, c))
+visited.add(tuple(sorted((a, b, c))))
 queue = deque([(a, b, c)])
 flag = False
 
@@ -14,31 +14,31 @@ while queue:
         break
 
     if x > y:
-        if (x - y, y + y, z) not in visited:
-            visited.add((x - y, y + y, z))
-            queue.append(((x - y, y + y, z)))
+        if tuple(sorted((x - y, y + y, z))) not in visited:
+            visited.add(tuple(sorted((x - y, y + y, z))))
+            queue.append((x - y, y + y, z))
     elif x < y:
-        if (x + x, y - x, z) not in visited:
-            visited.add((x + x, y - x, z))
-            queue.append(((x + x, y - x, z)))
+        if tuple(sorted((x + x, y - x, z))) not in visited:
+            visited.add(tuple(sorted((x + x, y - x, z))))
+            queue.append((x + x, y - x, z))
 
     if x > z:
-        if (x - z, y, z + z) not in visited:
-            visited.add((x - z, y, z + z))
-            queue.append(((x - z, y, z + z)))
+        if tuple(sorted((x - z, y, z + z))) not in visited:
+            visited.add(tuple(sorted((x - z, y, z + z))))
+            queue.append((x - z, y, z + z))
     elif x < z:
-        if (x + x, y, z - x) not in visited:
-            visited.add((x + x, y, z - x))
-            queue.append(((x + x, y, z - x)))
+        if tuple(sorted((x + x, y, z - x))) not in visited:
+            visited.add(tuple(sorted((x + x, y, z - x))))
+            queue.append((x + x, y, z - x))
 
     if y > z:
-        if (x, y - z, z + z) not in visited:
-            visited.add((x, y - z, z + z))
-            queue.append(((x, y - z, z + z)))
+        if tuple(sorted((x, y - z, z + z))) not in visited:
+            visited.add(tuple(sorted((x, y - z, z + z))))
+            queue.append((x, y - z, z + z))
     elif y < z:
-        if (x, y + y, z - y) not in visited:
-            visited.add((x, y + y, z - y))
-            queue.append(((x, y + y, z - y)))
+        if tuple(sorted((x, y + y, z - y))) not in visited:
+            visited.add(tuple(sorted((x, y + y, z - y))))
+            queue.append((x, y + y, z - y))
 
 if flag == False:
     print(0)
