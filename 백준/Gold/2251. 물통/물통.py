a6f5086ed942894a88f52f0c4843d903fb2bfacf
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-04 15:48:03 +0900

    [Gold IV] Title: 물통, Time: 108 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.py
+++ 백준/Gold/2251. 물통/물통.py
@@ -15,31 +15,46 @@ while queue:
     if x == 0:
         answer.add(z)
 
-    # X => Y
-    if (max(0, x - (b - y)), min(b, y + (x - max(0, x - (b - y)))), z) not in visited:
-        visited.add((max(0, x - (b - y)), min(b, y + (x - max(0, x - (b - y)))), z))
-        queue.append((max(0, x - (b - y)), min(b, y + (x - max(0, x - (b - y)))), z))
-    # Y => X
-    if (min(a, x + (y - max(0, y - (a - x)))), max(0, y - (a - x)), z) not in visited:
-        visited.add((min(a, x + (y - max(0, y - (a - x)))), max(0, y - (a - x)), z))
-        queue.append((min(a, x + (y - max(0, y - (a - x)))), max(0, y - (a - x)), z))
+    # 1) X => Y
+    move = min(x, b - y)
+    new_x, new_y, new_z = x - move, y + move, z
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
 
-    # X => Z
-    if (max(0, x - (c - z)), y, min(c, z + (x - max(0, x - (c - z))))) not in visited:
-        visited.add((max(0, x - (c - z)), y, min(c, z + (x - max(0, x - (c - z))))))
-        queue.append((max(0, x - (c - z)), y, min(c, z + (x - max(0, x - (c - z))))))
-    # Z => X
-    if (min(a, x + (z - max(0, z - (a - x)))), y, max(0, z - (a - x))) not in visited:
-        visited.add((min(a, x + (z - max(0, z - (a - x)))), y, max(0, z - (a - x))))
-        queue.append((min(a, x + (z - max(0, z - (a - x)))), y, max(0, z - (a - x))))
+    # 2) Y => X
+    move = min(y, a - x)
+    new_x, new_y, new_z = x + move, y - move, z
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
 
-    # Z => Y
-    if (x, min(b, y + (z - max(0, z - (b - y)))), max(0, z - (b - y))) not in visited:
-        visited.add((x, min(b, y + (z - max(0, z - (b - y)))), max(0, z - (b - y))))
-        queue.append((x, min(b, y + (z - max(0, z - (b - y)))), max(0, z - (b - y))))
-    # Y => Z
-    if (x, max(0, y - (c - z)), min(c, z + (y - max(0, y - (c - z))))) not in visited:
-        visited.add((x, max(0, y - (c - z)), min(c, z + (y - max(0, y - (c - z))))))
-        queue.append((x, max(0, y - (c - z)), min(c, z + (y - max(0, y - (c - z))))))
+    # 3) X => Z
+    move = min(x, c - z)
+    new_x, new_y, new_z = x - move, y, z + move
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
+
+    # 4) Z => X
+    move = min(z, a - x)
+    new_x, new_y, new_z = x + move, y, z - move
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
+
+    # 5) Z => Y
+    move = min(z, b - y)
+    new_x, new_y, new_z = x, y + move, z - move
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
+
+    # 6) Y => Z
+    move = min(y, c - z)
+    new_x, new_y, new_z = x, y - move, z + move
+    if (new_x, new_y, new_z) not in visited:
+        visited.add((new_x, new_y, new_z))
+        queue.append((new_x, new_y, new_z))
 
 print(*sorted(list(answer)))
