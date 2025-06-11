fb936e57b52eb6fbb219b40bd4f7cafecc30cc26
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-11 15:32:34 +0900

    [level 2] Title: 피로도, Time: 47.71 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 66.75 ms
+메모리: 9.11 MB, 시간: 47.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 18일 17:04:14
+2025년 06월 11일 15:32:29
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -2,21 +2,17 @@ answer = -1
 
 
 def solution(k, dungeons):
-    global answer
     visited = [False for _ in range(len(dungeons))]
 
     def dfs(hp, count):
         global answer
+        answer = max(answer, count)
 
         for i in range(len(dungeons)):
-            if visited[i] == False:
-                current_dungeon = dungeons[i]
-                if hp >= current_dungeon[0]:
-                    visited[i] = True
-                    dfs(hp - current_dungeon[1], count + 1)
-                    visited[i] = False
-
-        answer = max(answer, count)
+            if visited[i] == False and hp >= dungeons[i][0]:
+                visited[i] = True
+                dfs(hp - dungeons[i][1], count + 1)
+                visited[i] = False
 
     dfs(k, 0)
 
