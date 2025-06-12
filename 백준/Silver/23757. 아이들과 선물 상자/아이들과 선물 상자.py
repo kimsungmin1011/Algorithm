b59864a6db6580b395631625cb9f4c71f0e109f6
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-12 22:03:01 +0900

    [Silver II] Title: 아이들과 선물 상자, Time: 204 ms, Memory: 130976 KB -BaekjoonHub

---
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
@@ -0,0 +1,27 @@
+import heapq
+
+n, m = map(int, input().split())
+
+box = list(map(int, input().split()))  # 각 상자별 장난감 개수
+box_q = []  # 학생들이 원하는 장난감 개수
+for i in box:
+    heapq.heappush(box_q, -i)
+
+want = list(map(int, input().split()))
+
+flag = True
+
+for currnet in want:
+    out = heapq.heappop(box_q)
+    if currnet == -out:
+        heapq.heappush(box_q, 0)
+    elif currnet < -out:
+        heapq.heappush(box_q, -(-out - currnet))
+    elif currnet > -out:
+        flag = False
+        break
+
+if flag == True:
+    print(1)
+else:
+    print(0)
