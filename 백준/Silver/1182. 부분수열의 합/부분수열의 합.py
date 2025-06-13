906f19ca00a2ec5fb71ece42679ee61e4f608ff1
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-13 17:26:06 +0900

    [Silver II] Title: 부분수열의 합, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,18 @@
+def dfs(n, sm, cnt):
+    global ans
+    # 종료조건(n에 관련) + 정답관련 처리
+    if n == N:
+        if sm == S and cnt > 0:
+            ans += 1
+        return
+
+    # 하부함수 호출
+    dfs(n + 1, sm + lst[n], cnt + 1)  # 포함하는 경우
+    dfs(n + 1, sm, cnt)  # 포함하지 않는 경우
+
+
+N, S = map(int, input().split())
+lst = list(map(int, input().split()))
+ans = 0
+dfs(0, 0, 0)
+print(ans)
