4deab1e3f1ea655b402185ee9f6d69c072e56f75
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-15 15:08:36 +0900

    [Gold III] Title: 다리 만들기, Time: 208 ms, Memory: 115664 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,52 +1,61 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 n = int(input())
 graph = []
-for _ in range(n):
+
+for i in range(n):
     graph.append(list(map(int, input().split())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+visited1 = [[False for _ in range(n)] for _ in range(n)]  # 대륙 방문 체크
+visited2 = [[int(1e9) for _ in range(n)] for _ in range(n)]  # 바다 방문 체크
+min_distance = int(1e9)  # 대륙간 최단거리
 
+
+# 용도: 같은 육지 탐색해서 모두 방문한 다음 바다와 닿아있는 부분 큐에 넣고 다음 육지까지 탐색
 def bfs(a, b):
-    queue = deque([(a, b)])
-    queue2 = deque([])
-    visited2 = [[False for _ in range(n)] for _ in range(n)]  # 0(바다) 방문리스트
-    while queue:  # 먼저 현재 섬 탐색
-        x, y = queue.popleft()
+    global min_distance
+    queue1 = deque([(a, b)])
+    queue2 = deque()
+    visited1[a][b] = True
+
+    # 우선 같은 대륙을 모두 탐색한다
+    while queue1:
+        x, y = queue1.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False:
-                if graph[nx][ny] == 1:
-                    visited[nx][ny] = True
-                    queue.append((nx, ny))
-                if graph[nx][ny] == 0:  # 바다와 맞닿아 있다면
-                    queue2.append((x, y, 0))  # 다리 놓기 시작할 후보지
+            if 0 <= nx < n and 0 <= ny < n:
+                if visited1[nx][ny] == False:
+                    if graph[nx][ny] == 1:
+                        visited1[nx][ny] = True
+                        queue1.append((nx, ny))
+                    else:
+                        # 바다와 맞닿아 있는 부분은 큐2에 넣어준다
+                        queue2.append((x, y, 0))
 
-    while queue2:  # 현재 섬 탐색 끝나면 다리 놓기 시작
-        x2, y2, t = queue2.popleft()
+    # 이후 현재 대륙에서 다른 대륙까지의 거리를 측정한다
+    while queue2:
+        x, y, distance = queue2.popleft()
         for i in range(4):
-            nx2, ny2 = x2 + dx[i], y2 + dy[i]
-            if 0 <= nx2 < n and 0 <= ny2 < n:
-                if graph[nx2][ny2] == 1 and visited[nx2][ny2] == False:
-                    return t  # 방문하지 않은 다른 섬과 붙었다면 다리 길이 return
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0 and visited2[nx][ny] > distance + 1:
+                    # 바다 방문
+                    visited2[nx][ny] = distance + 1
+                    queue2.append((nx, ny, distance + 1))
+                if graph[nx][ny] == 1 and visited1[nx][ny] == False:
+                    # 새로운 대륙 방문
+                    min_distance = min(min_distance, distance)
 
-                elif graph[nx2][ny2] == 0 and visited2[nx2][ny2] == False:
-                    visited2[nx2][ny2] = True
-                    queue2.append((nx2, ny2, t + 1))  # 바다에선 다리 계속 확장
 
-    return int(1e9)  # 모든 섬을 다 방문해서 마지막 섬에서 놓을 다리가 없을 때 return
-
-
-visited = [[False for _ in range(n)] for _ in range(n)]  # 육지(1) 방문리스트
-
-min_value = int(1e9)
 for i in range(n):
-    for i2 in range(n):
-        if graph[i][i2] == 1 and visited[i][i2] == False:  # 방문하지 않은 섬이 있다면
-            visited[i][i2] = True
-            # 해당 섬에서 놓을 수 있는 가장 짧은 다리 길이로 최솟값 갱신
-            min_value = min(min_value, bfs(i, i2))
+    for j in range(n):
+        if visited1[i][j] == False and graph[i][j] == 1:
+            bfs(i, j)
 
-print(min_value)
+print(min_distance)
