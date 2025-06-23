ed4135d5084fec0af1b1fdccb322448d7bbcef83
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-23 15:58:43 +0900

    [level 2] Title: N-Queen, Time: 1060.68 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -3,32 +3,34 @@ answer = 0
 
 def solution(n):
     global answer
-    daekaksunright = [False] * (2 * n)  # / 대각선 방문 배열 (범위: 0 ~ 2*n)
-    daekaksunleft = [False] * (2 * n)  # \ 대각선 방문 배열 (범위: -n ~ n)
+    daekaksunright = [False] * (2 * n)  # / 대각선 방문 배열
+    daekaksunleft = [False] * (2 * n)  # \ 대각선 방문 배열
     yeol = [False] * n  # 열 방문 배열
 
     def dfs(idx):
         global answer
+        # idx가 n과 같으면 n개의 퀸을 모두 놓은 것이므로 경우의 수 +1
         if idx == n:
             answer += 1
             return
 
-        # 행(idx)을 기준으로 이전에 탐색하던 행의 다음 행 탐색 (각 행마다 최소 한 개의 퀸이 있어야 함)
         for j in range(n):
             if (
-                daekaksunleft[idx - j] == False
-                and daekaksunright[idx + j] == False
-                and yeol[j] == False
+                not daekaksunleft[idx - j]
+                and not daekaksunright[idx + j]
+                and not yeol[j]
             ):
+                # 퀸 놓기
                 daekaksunleft[idx - j] = True
                 daekaksunright[idx + j] = True
                 yeol[j] = True
-                dfs(idx + 1)
+
+                dfs(idx + 1)  # 인자는 하나만!
+
                 # 백트래킹 원복
                 daekaksunleft[idx - j] = False
                 daekaksunright[idx + j] = False
                 yeol[j] = False
 
     dfs(0)
-
     return answer
