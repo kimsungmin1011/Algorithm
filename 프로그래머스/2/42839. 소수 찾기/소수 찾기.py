f9f53a0f9bebc418eea39898cf570344173c5cb6
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-25 16:14:12 +0900

    [level 2] Title: 소수 찾기, Time: 20.13 ms, Memory: 9.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,44 @@
+import math
+
+answer = 0
+
+
+def sosu(number):
+    flag = True
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            flag = False
+            break
+
+    return flag
+
+
+def solution(numbers):
+    numbers = list(numbers)
+    visited = [False for _ in range(len(numbers))]
+    current_list = []
+    jungbok = set()
+
+    def dfs():
+        global answer
+        if current_list:
+            current_nunmber = int("".join(current_list))
+        if current_list and current_nunmber > 1:
+            if current_nunmber not in jungbok and sosu(current_nunmber):
+                jungbok.add(current_nunmber)
+                answer += 1
+
+        if len(current_list) == len(numbers):
+            return
+
+        for i in range(len(numbers)):
+            if visited[i] == False:
+                visited[i] = True
+                current_list.append(numbers[i])
+                dfs()
+                current_list.pop()
+                visited[i] = False
+
+    dfs()
+
+    return answer
