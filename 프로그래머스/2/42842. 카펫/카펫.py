a5e3cc32e5a326118f69f3e46890ef0bbf91834a
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-06-25 19:14:54 +0900

    [level 2] Title: 카펫, Time: 0.73 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,25 @@
+# 주어진 갈색(brown)과 노란색(yellow) 격자의 수를 이용해 카펫의 가로, 세로 크기를 구하는 함수
 def solution(brown, yellow):
     answer = []
-    karosaerosum = (brown + 4) // 2
+    # 카펫의 가로 + 세로 값 (가로는 세로 이상)
+    # 공식 유도: 전체 둘레 = brown + 4, 따라서 (가로 + 세로) = (brown + 4) // 2
+    karosaero_sum = (brown + 4) // 2
+    # 가능한 가로, 세로 쌍 후보 저장
     karosaero_candidate = []
 
-    for i in range(1, karosaerosum):
-        if i >= karosaerosum - i:
-            karosaero_candidate.append((i, karosaerosum - i))
+    # 가능한 가로와 세로 쌍을 찾기 위한 반복문
+    for i in range(1, karosaero_sum):
+        # 조건: 가로(x)가 세로(y)보다 크거나 같아야 함
+        if i >= karosaero_sum - i:
+            # 가로(x), 세로(y) 후보 추가
+            karosaero_candidate.append((i, karosaero_sum - i))
 
+    # 모든 후보 쌍에 대해 유효한 조합인지 확인
     for x, y in karosaero_candidate:
+        # 조건: 전체 넓이에서 brown과 yellow를 뺀 값이 0이면 조건에 부합
+        # 즉, 전체 넓이 == brown + yellow
         if x * y - brown - yellow == 0:
+            # 정답: 조건을 만족하는 가로, 세로 쌍 저장
             answer = [x, y]
             break
 
