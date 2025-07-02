d442333eee0ba723d55c2dcee062de838e77f67d
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-02 18:24:31 +0900

    [Silver IV] Title: 문자열, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.py
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -1,13 +1,15 @@
-first, last = input().split()
+a, b = input().split()  # 문자열 A, B 입력 받음
+min_diff = 51  # 차이 최댓값보다 큰 수로 초기화 (최대 A길이 = 50)
 
-min_count = float("inf")
+# B 위에 A를 덮을 수 있는 모든 위치에서 비교해봄
+for start in range(len(b) - len(a) + 1):
+    diff = 0  # 이 위치에서 A랑 B가 다른 글자 수
 
-# A를 B에 맞춰 이동시키며 비교
-for i in range(len(last) - len(first) + 1):
-    count = 0
-    for j in range(len(first)):
-        if first[j] != last[i + j]:
-            count += 1
-    min_count = min(min_count, count)
+    # A를 한 글자씩 B랑 비교
+    for j in range(len(a)):
+        if a[j] != b[start + j]:  # 글자가 다르면
+            diff += 1  # 차이 +1
 
-print(min_count)
+    min_diff = min(min_diff, diff)  # 최소 차이 저장
+
+print(min_diff)  # 정답 출력
