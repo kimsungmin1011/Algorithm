c2b756b944f927643a35304ed7dda76b6947472e
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-12 19:47:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.75 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,7 +1,9 @@
 def solution(new_id):
+    # 신규 아이디 추천을 위한 알고리즘 구현
     answer = ""
     new_list = []
 
+    # 1단계: new_id의 모든 문자를 순회하며 소문자로 변환하고, 허용된 문자만 필터링하여 new_list에 추가
     for i in new_id:
         if 65 <= ord(i) <= 90:
             i = chr(ord(i) + 32)
@@ -18,6 +20,7 @@ def solution(new_id):
     new_id = new_list[:]
     new_list = [new_id[0]]
 
+    # 2단계: 연속된 '.'를 하나로 치환하여 new_list에 추가
     for i in range(1, len(new_id)):
         if new_id[i] == "." and new_id[i - 1] == ".":
             continue
@@ -27,6 +30,7 @@ def solution(new_id):
     new_id = new_list[:]
     new_list = []
 
+    # 3단계: 시작과 끝에 위치한 '.'을 제거
     for i in range(len(new_id)):
         if (i == 0 or i == len(new_id) - 1) and new_id[i] == ".":
             continue
@@ -36,20 +40,18 @@ def solution(new_id):
     new_id = new_list[:]
     new_list = []
 
+    # 4단계: new_id가 빈 문자열인 경우 'a'를 추가
     if len(new_id) == 0:
         new_id.append("a")
 
-    for i in range(len(new_id)):
-        if i >= 15:
-            continue
-        else:
-            new_list.append(new_id[i])
+    # 5단계: new_id의 첫 15개 문자만 남기기
+    new_id = new_id[:15]
 
-    new_id = new_list[:]
-
+    # 만약 제거 후 끝에 마침표가 있다면 제거한다
     if new_id[-1] == ".":
         new_id.pop()
 
+    # 6단계: 아이디 길이가 3 미만일 경우 마지막 문자를 반복하여 길이를 3으로 맞추기
     while len(new_id) < 3:
         new_id.append(new_id[-1])
 
