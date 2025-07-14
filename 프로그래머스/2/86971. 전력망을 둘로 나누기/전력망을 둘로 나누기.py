b25f05bf7202998fe1a200e7f0d9e89597b08313
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-14 16:19:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.30 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,43 +1,33 @@
-min_diff = int(1e9)
+def dfs(graph, visited, node):
+    count = 1
+    for next in graph[node]:
+        if visited[next] == False:
+            visited[next] = True
+            count += dfs(graph, visited, next)
+
+    return count
 
 
 def solution(n, wires):
-    global min_diff
-    graph = [[] for _ in range(n + 1)]
+    answer = int(1e9)
 
-    for i, j in wires:
-        graph[i].append(j)
-        graph[j].append(i)
-
-    for i in range(len(wires)):
-        a, b = wires[i]
+    for cut in range(n - 1):
         visited = [False for _ in range(n + 1)]
+        graph = [[] for _ in range(n + 1)]
 
-        def dfs(index):
-            count = 1
-            for next in graph[index]:
-                if (index == a and next == b) or (index == b and next == a):
-                    continue
-                if visited[next] == False:
-                    visited[next] = True
-                    count += dfs(next)
+        for i in range(n - 1):
+            if i == cut:
+                continue
+            x, y = wires[i]
+            graph[x].append(y)
+            graph[y].append(x)
 
-            return count
+        count_list = []
+        for i in range(1, n + 1):
+            if visited[i] == False:
+                visited[i] = True
+                count_list.append(dfs(graph, visited, i))
 
-        c_count = 0
-        flag = 0
+        answer = min(answer, abs(count_list[0] - count_list[1]))
 
-        for i2 in range(1, n + 1):
-            if visited[i2] == False:
-                flag += 1
-                visited[i2] = True
-                if flag == 1:
-                    number = dfs(i2)
-                    c_count = number
-                elif flag == 2:
-                    number = dfs(i2)
-                    c_count -= number
-
-        min_diff = min(min_diff, abs(c_count))
-
-    return min_diff
+    return answer
