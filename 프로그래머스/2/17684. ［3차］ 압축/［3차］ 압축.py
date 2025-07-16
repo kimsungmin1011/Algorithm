b99ecb926b6d4774ce096deff40b39bc19b44b91
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-16 19:29:55 +0900

    [level 2] Title: [3차] 압축, Time: 0.47 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,51 +1,30 @@
 def solution(msg):
+    # LZW 압축 알고리즘 최적화 버전: 해시 맵(dict) 사용
     answer = []
-    word_list = [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-    ]
+    # 사전 초기화: A-Z 알파벳 (문자열 → 인덱스)
+    word_dict = {chr(ord("A") + i): i + 1 for i in range(26)}
+    next_idx = 27  # 다음에 추가될 사전 인덱스
+    idx = 0
+    n = len(msg)
 
-    final_i = -100
-    for i in range(len(msg)):
-        if i <= final_i:
-            continue
-        current = []
-        for j in range(i, len(msg)):
-            current.append(msg[j])
-            c_word = "".join(current)
-            if c_word not in word_list:
-                word_list.append(c_word)
-                current.pop()
-                c_word = "".join(current)
-                answer.append(word_list.index(c_word) + 1)
-                final_i = j - 1
-                break
-            elif j == len(msg) - 1 and c_word in word_list:
-                answer.append(word_list.index(c_word) + 1)
-                final_i = j
+    # 메시지 전체를 한 번에 훑으며 압축
+    while idx < n:
+        w = msg[idx]       # 현재까지 매칭된 문자열
+        j = idx + 1
+        # 가능한 한 긴 문자열을 찾는다
+        while j < n and (w + msg[j]) in word_dict:
+            w += msg[j]
+            j += 1
+
+        # 매칭된 w의 사전 인덱스 추가
+        answer.append(word_dict[w])
+
+        # 새롭게 발견된 문자열(w + 다음 문자)을 사전에 등록
+        if j < n:
+            word_dict[w + msg[j]] = next_idx
+            next_idx += 1
+
+        # 다음 탐색 시작 위치 갱신
+        idx = j
 
     return answer
