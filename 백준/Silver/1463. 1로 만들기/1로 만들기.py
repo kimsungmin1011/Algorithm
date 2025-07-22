de187630fdc5ea5e765c0f0db3c63a5a49285317
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-22 16:21:55 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 118772 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,13 @@
 n = int(input())
+dp = [int(1e9) for _ in range(n + 1)]
+dp[1] = 0  # i에서 출발하는 경우의 수
 
-dp = [0] * 1000001
-
-dp[2] = 1
-dp[3] = 1
-
-for i in range(4, n + 1):
-    dp[i] = dp[i - 1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
+for i in range(1, n):
+    if i * 3 <= n:
+        dp[i * 3] = min(dp[i * 3], dp[i] + 1)
+    if i * 2 <= n:
+        dp[i * 2] = min(dp[i * 2], dp[i] + 1)
+    if i + 1 <= n:
+        dp[i + 1] = min(dp[i + 1], dp[i] + 1)
 
 print(dp[n])
