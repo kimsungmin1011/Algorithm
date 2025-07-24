3276a398c8191bb1d27e4ef413ee98de518aef23
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-24 16:40:06 +0900

    [level 2] Title: 오픈채팅방, Time: 119.34 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?gad_source=1&gad_campaignid=22799790467&gbraid=0AAAAAC_c4nCSIWjJ3MSMxJybRNdxT7uEw&gclid=Cj0KCQjws4fEBhD-ARIsACC3d286Id2JADvQVc8Q2Gs4oXxHRb3a2UL-NzOFbT-vaKw1eG2XggEKdOAaAiPDEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 40.7 MB, 시간: 133.05 ms
+메모리: 40.6 MB, 시간: 119.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 06일 16:18:16
+2025년 07월 24일 16:40:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,19 +1,17 @@
 def solution(record):
     answer = []
-    user = {}
+    user_id = {}
 
     for i in record:
-        i = list(i.split(" "))
-        if i[0] == "Enter":
-            user[i[1]] = i[2]
-        elif i[0] == "Change":
-            user[i[1]] = i[2]
+        i = i.split(" ")
+        if i[0] == "Enter" or i[0] == "Change":
+            user_id[i[1]] = i[2]
 
     for i in record:
-        i = list(i.split(" "))
+        i = i.split(" ")
         if i[0] == "Enter":
-            answer.append(f"{user[i[1]]}님이 들어왔습니다.")
+            answer.append(user_id[i[1]] + "님이 들어왔습니다.")
         elif i[0] == "Leave":
-            answer.append(f"{user[i[1]]}님이 나갔습니다.")
+            answer.append(user_id[i[1]] + "님이 나갔습니다.")
 
     return answer
