a478257fb8aa14b68bf970a79020868c004fe21c
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-07-29 14:29:37 +0900

    [Gold V] Title: 0 만들기, Time: 212 ms, Memory: 114088 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,24 +1,22 @@
 t = int(input())
 
 
-def dfs(index, array1, array2):
+def dfs(index, array):
     if index == n:
-        if eval("".join(array1)) == 0:
-            answer.append(array2)
+        if eval(array.replace(" ", "")) == 0:
+            answer.append(array)
         return
 
-    dfs(index + 1, array1 + [f"+{index+1}"], array2 + f"+{index+1}")
-    dfs(index + 1, array1 + [f"-{index+1}"], array2 + f"-{index+1}")
-    new_n = array1.pop()
-    new_n += str(index + 1)
-    dfs(index + 1, array1 + [new_n], array2 + f" {index+1}")
+    dfs(index + 1, array + f"+{index+1}")
+    dfs(index + 1, array + f"-{index+1}")
+    dfs(index + 1, array + f" {index+1}")
 
 
 for _ in range(t):
     n = int(input())
     answer = []
 
-    dfs(1, ["1"], "1")
+    dfs(1, "1")
 
     for i in sorted(answer):
         print(i)
