62a371dd3a4474d389eaf2fdd6d352b1caff8fb4
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-08-18 16:29:32 +0900

    [level 2] Title: 괄호 회전하기, Time: 69.23 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?gad_source=1&gad_campaignid=22215033033&gbraid=0AAAAAC_c4nCCxi6MZmcdSYvmtaTGMNb2n&gclid=Cj0KCQjw-4XFBhCBARIsAAdNOkudyr9CN7w516saX02rcJ01taT-k292tFw-vTHUlbOaJQxtP1yP8LYaAqNvEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 96.10 ms
+메모리: 9.19 MB, 시간: 69.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 18:41:58
+2025년 08월 18일 16:29:28
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -3,50 +3,30 @@ from collections import deque
 
 def solution(s):
     answer = 0
-    count = -1
     queue = deque(s)
+    l = len(s)
 
-    while True:
-        count += 1
+    for _ in range(l):
+        a = queue.popleft()
+        queue.append(a)
 
-        if count == len(s):
-            break
-
+        c_queue = deque()
         flag = True
-        check = []
-
-        for i in list(queue):
-            if i == "[":
-                check.append("[")
-            elif i == "]":
-                if check and check[-1] == "[":
-                    check.pop()
+        for i in queue:
+            if i == "[" or i == "{" or i == "(":
+                c_queue.append(i)
+            else:
+                if c_queue and (
+                    (i == "]" and c_queue[-1] == "[")
+                    or (i == "}" and c_queue[-1] == "{")
+                    or (i == ")" and c_queue[-1] == "(")
+                ):
+                    c_queue.pop()
                 else:
                     flag = False
                     break
 
-            elif i == "(":
-                check.append("(")
-            elif i == ")":
-                if check and check[-1] == "(":
-                    check.pop()
-                else:
-                    flag = False
-                    break
-
-            elif i == "{":
-                check.append("{")
-            elif i == "}":
-                if check and check[-1] == "{":
-                    check.pop()
-                else:
-                    flag = False
-                    break
-
-        if len(check) == 0 and flag == True:
+        if flag == True and len(c_queue) == 0:
             answer += 1
-
-        left = queue.popleft()
-        queue.append(left)
 
     return answer
