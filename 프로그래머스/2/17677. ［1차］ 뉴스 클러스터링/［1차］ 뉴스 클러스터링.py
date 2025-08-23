6bbd841074173488b373c59dde62c8cc2454d2a9
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-08-23 13:37:02 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.61 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,94 +1,58 @@
+def calc(arr):
+    # 문자열에서 알파벳으로만 이루어진 2글자 조합(bigram)을 추출해 딕셔너리(다중집합)로 반환
+    cdict = {}
+    for i in range(2, len(arr) + 1):
+        # 연속된 두 글자를 추출
+        cut_arr = arr[i - 2 : i]
+
+        # 알파벳으로만 구성된 경우가 아니면 무시
+        if cut_arr.isalpha() == False:
+            continue
+
+        # 처음 나온 bigram이면 1로 초기화
+        if cut_arr not in cdict:
+            cdict[cut_arr] = 1
+        # 이미 존재하면 개수 +1
+        else:
+            cdict[cut_arr] += 1
+
+    return cdict
+
+
 def solution(str1, str2):
     answer = 0
+    # str1/str2에서 알파벳만으로 된 2글자 조합을 추출해 다중집합 딕셔너리를 만든다
     str1 = str1.lower()
     str2 = str2.lower()
 
-    hapdict = {}
-    hapdict2 = {}
-    kyodict = {}
-    kyodict2 = {}
+    dict1 = calc(str1)
+    dict2 = calc(str2)
 
-    for i in range(2, len(str1) + 1):
-        arr = str1[i - 2 : i]
-        flag = True
-        for i in arr:
-            if i.isalpha() == False:
-                flag = False
-                break
-        if flag == False:
-            continue
-
-        if arr not in hapdict:
-            hapdict[arr] = 1
-        else:
-            hapdict[arr] += 1
-
-    for i in range(2, len(str2) + 1):
-        arr = str2[i - 2 : i]
-        flag = True
-        for i in arr:
-            if i.isalpha() == False:
-                flag = False
-                break
-        if flag == False:
-            continue
-
-        if arr not in hapdict2:
-            hapdict2[arr] = 1
-        else:
-            hapdict2[arr] += 1
-
+    # dict1과 dict2에 존재하는 모든 원소를 포함, 각 원소 개수는 두 딕셔너리에서의 최대값
     final_hap = dict()
-    for i in hapdict:
-        if i in hapdict2:
-            final_hap[i] = max(hapdict[i], hapdict2[i])
+    for i in dict1:
+        if i in dict2:
+            final_hap[i] = max(dict1[i], dict2[i])
         else:
-            final_hap[i] = hapdict[i]
+            final_hap[i] = dict1[i]
 
-    for i in hapdict2:
+    for i in dict2:
         if i not in final_hap:
-            final_hap[i] = hapdict2[i]
+            final_hap[i] = dict2[i]
 
-    for i in range(2, len(str1) + 1):
-        arr = str1[i - 2 : i]
-        flag = True
-        for i in arr:
-            if i.isalpha() == False:
-                flag = False
-                break
-        if flag == False:
-            continue
-
-        if arr not in kyodict:
-            kyodict[arr] = 1
-        else:
-            kyodict[arr] += 1
-
-    for i in range(2, len(str2) + 1):
-        arr = str2[i - 2 : i]
-        flag = True
-        for i in arr:
-            if i.isalpha() == False:
-                flag = False
-                break
-        if flag == False:
-            continue
-
-        if arr not in kyodict2:
-            kyodict2[arr] = 1
-        else:
-            kyodict2[arr] += 1
-
+    # dict1과 dict2에 모두 존재하는 원소만 포함, 각 원소 개수는 두 딕셔너리에서의 최소값
     final_kyo = dict()
-    for i in kyodict:
-        if i in kyodict2:
-            final_kyo[i] = min(kyodict[i], kyodict2[i])
+    for i in dict1:
+        if i in dict2:
+            final_kyo[i] = min(dict1[i], dict2[i])
 
-    if sum(final_kyo.values()) != 0 and sum(final_hap.values()) != 0:
-        answer = sum(final_kyo.values()) / sum(final_hap.values()) * 65536 // 1
-    elif sum(final_kyo.values()) == 0 and sum(final_hap.values()) == 0:
+    # 교집합 크기 ÷ 합집합 크기 × 65536, 합집합이 비어있으면 65536 반환
+    sum_hap = sum(final_hap.values())
+    sum_kyo = sum(final_kyo.values())
+
+    if sum_hap == 0:
         answer = 65536
     else:
-        answer = 0
+        answer = int(sum_kyo / sum_hap * 65536)
 
     return answer
