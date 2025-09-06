29d4d31e42bd2b5d23bb530a3abf6ad2414db6b9
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-09-06 20:08:00 +0900

    [level 2] Title: 시소 짝꿍, Time: 6.99 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,24 +1,20 @@
 from collections import Counter
 
-
 def solution(weights):
     answer = 0
+    weights=Counter(weights)
     
-    # 1:1
-    counter = Counter(weights)
-    for key,value in counter.items():
-        if value>=2:
-            answer+= value *(value-1)//2
+    for i in weights:
+        if weights[i]>1:
+            answer+=(weights[i])*(weights[i]-1)//2
             
-    weights = set(weights) #1:1 구한 후 중복 제거  
+        if i*1.5 in weights:
+            answer+=weights[i]*weights[i*1.5]
+            
+        if i*2 in weights:
+            answer+=weights[i]*weights[i*2]
+            
+        if i*4/3 in weights:
+            answer+=weights[i]*weights[i*4/3]
     
-    #2:3, 2:4, 3:4
-    for w in weights:
-        if w*2/3 in weights:
-            answer+= counter[w*2/3] * counter[w]
-        if w*2/4 in weights:
-            answer+= counter[w*2/4] * counter[w]
-        if w*3/4 in weights:
-            answer+= counter[w*3/4] * counter[w]
-            
     return answer
