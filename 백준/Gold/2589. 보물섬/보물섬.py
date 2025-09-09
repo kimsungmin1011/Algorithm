0a47b0794cca61532ea91cb369684844719caaea
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-09-09 17:51:28 +0900

    [Gold V] Title: 보물섬, Time: 4652 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,39 +1,44 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
 
 n, m = map(int, input().split())
+
 graph = []
-for i in range(n):
-    graph.append(list(input()))
+for _ in range(n):
+    graph.append(input())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-max_distance = 0
 
+def bfs(a, b):
+    queue = deque([(a, b)])
+    visited = [[-1 for _ in range(m)] for _ in range(n)]
+    visited[a][b] = 0
 
-def bfs(x, y):
-    global max_distance
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    visited[x][y] = True
-    queue = deque([(x, y, 0)])
     while queue:
-        a, b, d = queue.popleft()
-        max_distance = max(max_distance, d)
-
+        x, y = queue.popleft()
         for i in range(4):
-            nx, ny = a + dx[i], b + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False:
-                if graph[nx][ny] == "L":
-                    visited[nx][ny] = True
-                    queue.append((nx, ny, d + 1))
+            nx, ny = x + dx[i], y + dy[i]
+            if (
+                0 <= nx < n
+                and 0 <= ny < m
+                and graph[nx][ny] == "L"
+                and visited[nx][ny] == -1
+            ):
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx, ny))
+
+    max_value = -1
+    for i in visited:
+        max_value = max(max_value, max(i))
+
+    return max_value
 
 
+answer = -1
 for i in range(n):
-    for i2 in range(m):
-        if graph[i][i2] == "L":
-            bfs(i, i2)
+    for j in range(m):
+        if graph[i][j] == "L":
+            answer = max(answer, bfs(i, j))
 
-print(max_distance)
+print(answer)
