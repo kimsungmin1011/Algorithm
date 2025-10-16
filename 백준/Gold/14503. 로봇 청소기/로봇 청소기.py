0617253cc0526c65f58f7ac5df8ac5ca6c167c58
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-10-16 23:51:19 +0900

    [Gold V] Title: 로봇 청소기, Time: 116 ms, Memory: 113500 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+n, m = map(int, input().split())
+r, c, d = map(int, input().split())
+
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+if d % 2 == 0:
+    queue = deque([(r, c, d, 1)])
+else:
+    if d == 1:
+        queue = deque([(r, c, 3, 1)])
+    else:
+        queue = deque([(r, c, 1, 1)])
+
+visited[r][c] = True
+
+max_count = 1
+
+while queue:
+    x, y, cd, count = queue.popleft()
+    max_count = max(max_count, count)
+
+    flag = False
+    # 인접 4칸 중에 아직 방문 안 한 칸 있으면 전진
+    for i in range(1, 5):
+        nd = (cd + i) % 4
+        nx, ny = x + dx[nd], y + dy[nd]
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 0 and visited[nx][ny] == False:
+                flag = True
+                visited[nx][ny] = True
+                queue.append((nx, ny, nd, count + 1))
+                break
+
+    # 없으면 방향 유지한 채 뒤로 빠꾸
+    if flag == False:
+        nx, ny = x - dx[cd], y - dy[cd]
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            # 만약 빠꾸한 칸이 아직 방문 안 한 칸이라면
+            if visited[nx][ny] == False:
+                visited[nx][ny] = True
+                queue.append((nx, ny, cd, count + 1))
+            else:
+                queue.append((nx, ny, cd, count))
+
+print(max_count)
