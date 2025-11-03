a2ad6bc7b1ee95541892745ea98f27dd28d572de
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-11-03 14:22:37 +0900

    [Gold V] Title: 기타 레슨, Time: 120 ms, Memory: 121180 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.py
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.py
@@ -1,28 +1,23 @@
 n, m = map(int, input().split())
 lesson = list(map(int, input().split()))
 
-left = 1
+left = max(lesson)
 right = sum(lesson)
-answer = sum(lesson)
+answer = 0
 
 while left <= right:
     mid = (left + right) // 2
     count = 1
     weight = 0
-    flag = True
 
     for i in range(n):
-        if lesson[i] > mid:
-            flag = False
-            break
-
         if weight + lesson[i] > mid:
             weight = 0
             count += 1
 
         weight += lesson[i]
 
-    if flag == False or count > m:
+    if count > m:
         left = mid + 1
     else:
         answer = mid
