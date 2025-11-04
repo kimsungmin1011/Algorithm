3170a640d086ddd7a918dc362525affd66999f6e
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-11-04 14:40:07 +0900

    [Gold IV] Title: 좋다, Time: 136 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -4,21 +4,21 @@ number.sort()
 answer = 0
 
 for i in range(n):
-    start = 0 if i != 0 else 1
-    end = n - 1 if i != n - 1 else n - 2
-
+    start = 0
+    end = n - 1
     while start < end:
+        if start == i:
+            start += 1
+            continue
+        elif end == i:
+            end -= 1
+            continue
+
         current = number[start] + number[end]
         if current > number[i]:
-            if end - 1 != i:
-                end -= 1
-            else:
-                end -= 2
+            end -= 1
         elif current < number[i]:
-            if start + 1 != i:
-                start += 1
-            else:
-                start += 2
+            start += 1
         else:
             answer += 1
             break
