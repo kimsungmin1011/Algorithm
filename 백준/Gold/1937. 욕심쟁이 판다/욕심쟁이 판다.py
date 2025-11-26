ee1f409f08fb88ced7212af1947cf85d5d140e7c
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-11-26 10:12:53 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 272 ms, Memory: 135556 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,41 +1,34 @@
-import sys
+n = int(input())
+graph = []
 
-sys.setrecursionlimit(10**6)
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-n = int(sys.stdin.readline())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0 for _ in range(n)] for _ in range(n)]
 
-# dp[x][y] = (x, y)에서 시작해서 이동할 수 있는 최대 일수(경로 길이)
-dp = [[-1 for _ in range(n)] for _ in range(n)]
-
-# 상, 하, 좌, 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def dfs(x, y):
-    # 이미 계산된 적 있으면 바로 반환 (메모이제이션)
-    if dp[x][y] != -1:
-        return dp[x][y]
-
-    # 최소 1일(해당 칸에서 시작)
-    dp[x][y] = 1
-
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
-            if dp[nx][ny] == -1:
+            if dp[nx][ny] == 0:
+                dp[nx][ny] = 1
                 dfs(nx, ny)
             dp[x][y] = max(dp[x][y], dp[nx][ny] + 1)
 
-    return dp[x][y]
 
-
-answer = 0
 for i in range(n):
     for j in range(n):
-        if dp[i][j] == -1:
+        if dp[i][j] == 0:
+            dp[i][j] = 1
             dfs(i, j)
-        answer = max(answer, dp[i][j])
 
-print(answer)
+max_value = -1
+
+for i in dp:
+    max_value = max(max_value, max(i))
+
+print(max_value)
