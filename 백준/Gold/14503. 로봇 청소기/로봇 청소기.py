c8b0b76281c238ab0572c0a00cf5987587b94baf
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2025-12-11 11:22:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 104 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,11 +1,14 @@
-from collections import deque
-
 n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-
+graph = []
 visited = [[False for _ in range(m)] for _ in range(n)]
 
-graph = []
+x, y, d = map(int, input().split())
+if d == 3:
+    d = 1
+elif d == 1:
+    d = 3
+
+visited[x][y] = True
 
 for i in range(n):
     graph.append(list(map(int, input().split())))
@@ -13,44 +16,34 @@ for i in range(n):
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
-queue = deque()
 
-if d == 1:
-    queue.append((r, c, 3, 1))
-elif d == 3:
-    queue.append((r, c, 1, 1))
-else:
-    queue.append((r, c, d, 1))
-
-visited[r][c] = True
-answer = 1
-
-while queue:
-    x, y, cd, count = queue.popleft()
+while True:
     flag = False
-
-    # 인접 4칸 중에 아직 방문 안 한 칸 있으면 전진
     for i in range(1, 5):
-        nd = (cd + i) % 4
+        nd = (d + i) % 4
         nx, ny = x + dx[nd], y + dy[nd]
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny] == 0 and visited[nx][ny] == False:
-                flag = True
-                visited[nx][ny] = True
-                queue.append((nx, ny, nd, count + 1))
-                break
+        if (
+            0 <= nx < n
+            and 0 <= ny < m
+            and graph[nx][ny] == 0
+            and visited[nx][ny] == False
+        ):
+            visited[nx][ny] = True
+            x, y, d = nx, ny, nd
+            flag = True
+            break
 
-    # 없으면 방향 유지한 채 뒤로 빠꾸
     if flag == False:
-        nx, ny = x - dx[cd], y - dy[cd]
+        nx, ny = x - dx[d], y - dy[d]
         if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-            # 만약 빠꾸한 칸이 아직 방문 안 한 칸이라면
-            if visited[nx][ny] == False:
-                visited[nx][ny] = True
-                queue.append((nx, ny, cd, count + 1))
-            else:
-                queue.append((nx, ny, cd, count))
+            x, y = nx, ny
         else:
-            answer = count
+            break
 
-print(answer)
+count = 0
+for i in range(n):
+    for j in range(m):
+        if visited[i][j] == True:
+            count += 1
+
+print(count)
