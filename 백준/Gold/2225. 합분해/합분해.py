b4866a2af6a44ac5307717ac8ab4e28882e3afdf
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-03-13 16:25:04 +0900

    [Gold V] Title: 합분해, Time: 252 ms, Memory: 114352 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,3 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+
 # 0부터 N까지의 정수 K개를 더해서 합이 N이 되는 경우의 수
 n, k = map(int, input().split())
 # dp[k][n] => 정수 K개를 더해서 합이 N이 되는 경우의 수
@@ -10,8 +14,8 @@ for i in range(n + 1):
 for i in range(2, k + 1):
     for j in range(n + 1):
         for t in range(j + 1):
-            # 정수 i개 더해서 j가 되는 경우의 수 += 정수 i-1개 더해 j-t가 되는 경우의 수
-            # t는 0~j
-            dp[i][j] += dp[i - 1][j - t]
+            # 정수 i개 더해서 j가 되는 경우의 수 += 정수 i-1개 더해 t가 되는 경우의 수
+            # t는 0 ~ 현재값 j
+            dp[i][j] += dp[i - 1][t]
 
 print(dp[k][n] % 1000000000)
