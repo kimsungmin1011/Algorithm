a4b07aa21cde7464276fd5fd3a341b1708d91f34
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-03-19 16:21:39 +0900

    [Silver I] Title: 오르막 수, Time: 112 ms, Memory: 110900 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,16 +1,13 @@
 n = int(input())
+# dp[i][j] = i번째 위치에 j의 숫자가 있는 경우의 수
+dp = [[0 for _ in range(10)] for _ in range(n + 1)]
 
-# dp[깊이][마지막 숫자] = 해당 깊이에서 해당 마지막 숫자를 가진 오르막 수의 개수
-dp = [[0 for i in range(10)] for _ in range(n)]
-
-# dp 초기값
 for i in range(10):
-    dp[0][i] += 1
+    dp[1][i] = 1
 
-for i in range(1, n):
-    for i2 in range(10):
-        for i3 in range(i2 + 1):
-            # 전 단계에서 마지막 숫자가 i2 이하인 모든 오르막 수의 개수를 더함
-            dp[i][i2] += dp[i - 1][i3]
+for i in range(2, n + 1):
+    for j in range(10):
+        for k in range(j + 1):
+            dp[i][j] += dp[i - 1][k]
 
-print(sum(dp[n - 1]) % 10007)  # 깊이가 n인 모든 오르막 수의 개수
+print(sum(dp[n]) % 10007)
