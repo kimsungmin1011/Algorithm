0d79913dd5b3d5598244d6af36a8314a3849cc2d
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-03-25 15:31:17 +0900

    [Silver III] Title: 돌 게임 3, Time: 108 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -5,13 +5,9 @@ dp = [False for _ in range(n + 1)]
 
 for i in range(n + 1):
     for j in [1, 3, 4]:
-        if 0 <= i - j <= n:
-            if dp[i - j] == False:
-                dp[i] = True
-                break
+        if dp[i] == False and i + j <= n:
+            dp[i + j] = True
 
-
-# 상근이가 n개인 상태에서 먼저 시작
 if dp[n] == True:
     print("SK")
 else:
