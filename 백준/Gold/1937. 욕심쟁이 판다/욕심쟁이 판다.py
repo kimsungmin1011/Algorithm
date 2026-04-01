b1ed80fede212b9be9ee6ed8d1d43b65aa262c47
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-04-01 15:41:12 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 284 ms, Memory: 135320 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,6 +1,6 @@
 n = int(input())
-graph = []
 
+graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
@@ -11,11 +11,11 @@ dy = [0, 0, -1, 1]
 
 
 def dfs(x, y):
+    dp[x][y] = 1
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
             if dp[nx][ny] == 0:
-                dp[nx][ny] = 1
                 dfs(nx, ny)
             dp[x][y] = max(dp[x][y], dp[nx][ny] + 1)
 
@@ -23,12 +23,11 @@ def dfs(x, y):
 for i in range(n):
     for j in range(n):
         if dp[i][j] == 0:
-            dp[i][j] = 1
             dfs(i, j)
 
-max_value = -1
-
-for i in dp:
-    max_value = max(max_value, max(i))
+max_value = 0
+for i in range(n):
+    for j in range(n):
+        max_value = max(max_value, dp[i][j])
 
 print(max_value)
