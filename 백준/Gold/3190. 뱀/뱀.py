98f442de1cb5e7f5d6553926709fc04ce9d9fb1b
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-04-05 15:04:58 +0900

    [Gold IV] Title: 뱀, Time: 100 ms, Memory: 110844 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,63 +1,45 @@
-from collections import deque
+n = int(input())  # 맵 크기
+k = int(input())  # 사과개수
 
-n = int(input())
-k = int(input())
-
-apple = set()
-
+graph = [[0 for _ in range(n)] for _ in range(n)]
+# 사과위치
 for _ in range(k):
     x, y = map(int, input().split())
-    x -= 1
-    y -= 1
-    apple.add((x, y))
+    graph[x - 1][y - 1] = 1
 
+# 방향 바꾸는 시간, 방향
+change = dict()
 l = int(input())
-change = {}
-
 for _ in range(l):
-    line = list(input().split())
-    change[int(line[0])] = line[1]
+    x, d = input().split()
+    change[int(x)] = d  # x초가 끝난 뒤 d 방향으로 바꿈
 
-
-def changedef(d, turn):
-    if turn == "L":
-        d -= 1
-        if d < 0:
-            d = 3
-    else:
-        d = (d + 1) % 4
-    return d
-
-
-time = 1
-sx, sy = 0, 0
-d = 1
-
-snake_list = deque([(0, 0)])
+cx, cy, cd, time = 0, 0, 0, 1
+snake = [(0, 0)]
+# 동,남,서,북
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
 while True:
-    if d == 0:
-        sx -= 1
-    elif d == 1:
-        sy += 1
-    elif d == 2:
-        sx += 1
-    elif d == 3:
-        sy -= 1
-
-    if sx >= n or sy >= n or sx < 0 or sy < 0 or (sx, sy) in snake_list:
+    nx, ny = cx + dx[cd], cy + dy[cd]
+    if (0 <= nx < n and 0 <= ny < n and (nx, ny) not in snake) != True:
         break
-    elif (sx, sy) in apple:
-        apple.remove((sx, sy))  # 사과는 먹으면 사라짐
-        snake_list.append((sx, sy))
-    else:
-        snake_list.append((sx, sy))
-        snake_list.popleft()
 
+    cx, cy = nx, ny  # 뱀 머리위치 이동
+    snake.append((cx, cy))
+    if graph[cx][cy] != 1:
+        snake.pop(0)
+    else:
+        graph[cx][cy] = 0
+
+    # time초가 끝난 후 방향 변환(방향 변환 정보에 있을 시)
     if time in change:
-        turn = change[time]
-        d = changedef(d, turn)
+        if change[time] == "L":
+            cd = (cd - 1) % 4
+        if change[time] == "D":
+            cd = (cd + 1) % 4
 
     time += 1
+
 
 print(time)
