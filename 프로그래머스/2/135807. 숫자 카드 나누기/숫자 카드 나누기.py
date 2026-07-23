92393b67ac9f7f395ca96854a9a52b51f77a0ac0
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-07-23 13:57:42 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 60.16 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,23 @@
+from math import gcd
+
+
+def solution(arrayA, arrayB):
+    def get_gcd(numbers):
+        result = numbers[0]
+
+        for number in numbers[1:]:
+            result = gcd(result, number)
+
+        return result
+
+    def is_valid(candidate, other_array):
+        # 상대방의 모든 카드가 candidate로 나누어지지 않아야 함
+        return all(number % candidate != 0 for number in other_array)
+
+    gcd_a = get_gcd(arrayA)
+    gcd_b = get_gcd(arrayB)
+
+    answer_a = gcd_a if is_valid(gcd_a, arrayB) else 0
+    answer_b = gcd_b if is_valid(gcd_b, arrayA) else 0
+
+    return max(answer_a, answer_b)
