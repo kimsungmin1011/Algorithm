be71037485ce1cab6c91ffe7f81dd40a7a67fd1f
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-08-28 19:03:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 340.99 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,35 +1,20 @@
+from collections import Counter
+
 def solution(topping):
     answer = 0
-
-    left = {}   # 왼쪽 토핑 개수
-    right = {}  # 오른쪽 토핑 개수
-
-    # 처음에는 모든 토핑이 오른쪽에 있음
-    for t in topping:
-        if t not in right:
-            right[t] = 1
+    ldict=dict()
+    rdict=Counter(topping)
+    
+    for i in topping:
+        if i not in ldict:
+            ldict[i]=1
         else:
-            right[t] += 1
-
-    # 순서대로 잘라보기
-    for i in range(len(topping)):
-        now = topping[i]
-
-        # 현재 토핑을 왼쪽으로 이동
-        if now not in left:
-            left[now] = 1
-        else:
-            left[now] += 1
-
-        # 오른쪽에서는 현재 토핑 하나 제거
-        right[now] -= 1
-
-        # 오른쪽에 더 이상 해당 토핑이 없으면 종류에서 제거
-        if right[now] == 0:
-            del right[now]
-
-        # 왼쪽 토핑 종류 수 == 오른쪽 토핑 종류 수
-        if len(left) == len(right):
-            answer += 1
-
+            ldict[i]+=1
+        rdict[i]-=1
+        if rdict[i]==0:
+            del rdict[i]
+        
+        if len(ldict)==len(rdict):
+            answer+=1
+        
     return answer
