4f0eaf9b878f5171f9abd74cf4508c90dfc5b200
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-09-09 17:22:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.48 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,24 @@
 from collections import deque
 
-
 def solution(maps):
     answer = 0
-    dy = [-1, 1, 0, 0]
-    dx = [0, 0, -1, 1]
-
-    n = len(maps)
-    m = len(maps[0])
-    visited = [[-1 for _ in range(m)] for _ in range(n)]
-
-    def bfs():
-        queue = deque([(0, 0)])
-        visited[0][0] = 1
-        while queue:
-            x, y = queue.popleft()
-            if x == n - 1 and y == m - 1:
-                return visited[n - 1][m - 1]
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m:
-                    if maps[nx][ny] == 1 and visited[nx][ny] == -1:
-                        visited[nx][ny] = visited[x][y] + 1
-                        queue.append((nx, ny))
-        return -1
-
-    answer = bfs()
-
-    return answer
+    queue=deque([(0,0,1)])
+    n,m=len(maps),len(maps[0])
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+    visited=[[False for _ in range(m)] for _ in range(n)]
+    
+    while queue:
+        x,y,t=queue.popleft()
+        visited[x][y]=True
+        
+        if x==n-1 and y==m-1:
+            return t
+        
+        for i in range(4):
+            nx,ny=x+dx[i],y+dy[i]
+            if 0<=nx<n and 0<=ny<m and visited[nx][ny]==False and maps[nx][ny]==1:
+                visited[nx][ny]=True
+                queue.append((nx,ny,t+1))
+            
+    return -1
