8e3eaf30475a768f6ecf1ed4500a3acbc6bfb484
Sungmin Kim <122242600+kimsungmin1011@users.noreply.github.com>
2026-09-15 19:42:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.40 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,33 +1,46 @@
-def dfs(graph, visited, node):
-    count = 1
-    for next in graph[node]:
-        if visited[next] == False:
-            visited[next] = True
-            count += dfs(graph, visited, next)
-
-    return count
-
+from collections import deque
 
 def solution(n, wires):
-    answer = int(1e9)
-
-    for cut in range(n - 1):
-        visited = [False for _ in range(n + 1)]
-        graph = [[] for _ in range(n + 1)]
-
-        for i in range(n - 1):
-            if i == cut:
+    answer = int(1e8)
+    
+    def bfs(idx,visited):
+        q=deque([idx])
+        count=1
+        while q:
+            current=q.popleft()
+            for next in graph[current]:
+                if visited[next]==False:
+                    visited[next]=True
+                    q.append(next)
+                    count+=1
+        return count
+    
+    
+    for i in range(len(wires)):
+        graph=[[] for _ in range(n+1)]
+        # 전선 연결
+        for j in range(len(wires)):
+            # 전선 하나씩 끊어본다
+            if i==j:
                 continue
-            x, y = wires[i]
-            graph[x].append(y)
-            graph[y].append(x)
-
-        count_list = []
-        for i in range(1, n + 1):
-            if visited[i] == False:
-                visited[i] = True
-                count_list.append(dfs(graph, visited, i))
-
-        answer = min(answer, abs(count_list[0] - count_list[1]))
-
+            graph[wires[j][0]].append(wires[j][1])
+            graph[wires[j][1]].append(wires[j][0])
+        
+        # 네트워크 하나씩 탐색
+        visited=[False for _ in range(n+1)]
+        flag=False
+        first,secound=0,0
+        for k in range(1,n+1):
+            if visited[k]==False:
+                visited[k]=True
+                # 첫번째 네트워크
+                if flag==False:
+                    flag=True
+                    first=bfs(k,visited)
+                # 두번째 네트워크
+                else:
+                    secound=bfs(k,visited)
+                    
+        answer=min(answer,abs(first-secound))
+        
     return answer
